c64ae89259ef72a10f32a8886e6f2577b4141774
Kira Yagami <vutung1991@gmail.com>
2023-01-25 17:31:41 +0000

    commit

--- src/main/java/Comparators.java
+++ src/main/java/Comparators.java
@@ -24,7 +24,12 @@ public class Comparators {
      * @return true if a is equal to b, false otherwise.
      */
     public boolean isEqual(int a, int b){
-        return false;
+        if(a == b){
+            return true;
+        }else{
+            return false;
+        }
+        
     }
 
     /**
@@ -33,7 +38,11 @@ public class Comparators {
      * @return true if a is greater than b, false otherwise.
      */
     public boolean greaterThan(int a, int b){
+        if(a > b){
+            return true;
+        }else{
         return false;
+        }
     }
     /**
      * @param a the first number to be compared.
@@ -41,7 +50,11 @@ public class Comparators {
      * @return true if a is less than b, false otherwise.
      */
     public boolean lessThan(int a, int b){
-        return false;
+        if(a < b){
+            return true;
+        }else{
+            return false;
+        }
     }
     /**
      * @param a the first number to be compared.
@@ -49,7 +62,12 @@ public class Comparators {
      * @return true if a is greater than or equal to b, false otherwise.
      */
     public boolean greaterThanOrEqualTo(int a, int b){
-        return false;
+        if(a >= b){
+            return true;
+        }else{
+            return false;
+        }
+        
     }
     /**
      * @param a the first number to be compared.
@@ -57,7 +75,11 @@ public class Comparators {
      * @return true if a is less than or equal to b, false otherwise.
      */
     public boolean lessThanOrEqualTo(int a, int b){
-        return false;
+        if(a <= b){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     /**
@@ -66,6 +88,11 @@ public class Comparators {
      * @return true if a is not equal to b, false otherwise.
      */
     public boolean notEqualTo(int a, int b){
-        return false;
+        if(a != b){
+            return true;
+        }else{
+            return false;
+        }
+        
     }
 }
